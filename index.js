be2c1df73bafad7d23f7164ed95d5a8862538369
Jolie Hobloss <jolieshobloss@gmail.com>
2020-12-12 09:22:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 // Add your functions here
+function mapNegative(array, fx){
+  let arr=[]
+  for(let i=0;i <array.length;i++){
+    arr.push(fx(array[i]));
+    
+  }
+  return arr;
+}
+
+function reduce(array, fx, start){
+ let a= (!!start) ? start : array[0];
+ let b= (!!start) ? 0 : 1;
+ 
+ for (let i ; i< array.length;i++){
+    a = fx(array[i], a)
+ }
+ 
+ return a;
+}
 
 
 
